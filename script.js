20fcbbde7a3bfc25f19ed8b33d794acd843d67f3
saicharan0977-rgb <saicharan0977@gmail.com>
2026-02-04 16:47:33 +0000

    Revert "redo"
    This reverts commit 9f13ba4464af3c9a0ebd520b21eb6ec400a6a9be.

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const display = document.querySelector(".display");
 const nums = document.querySelector(".nums");
-const operator = document.querySelector(".opS");
-
+let digit1=0;
+let digit2=0;
+let temp;
+let dotCount=0;
+let digitCount=0;
+let opCount=0;
+nums.addEventListener("click", (e)=>{
+  if(e.target.matches(".num")&& digitCount<=7 ){
+   display.textContent+= e.target.textContent;
+   digitCount++;
+  }
+  else if(e.target.matches(".clear")){
+     digit1=0;
+     digit2=0;
+     temp=0; 
+     dotCount=0;
+     digitCount=0;
+     display.textContent="";
+  }
+  else if(e.target.matches(".once")){
+  if(dotCount==0&&digitCount>0){
+    display.textContent+=e.target.textContent;
+    dotCount++;
+  }
+  }
+});
 
 
